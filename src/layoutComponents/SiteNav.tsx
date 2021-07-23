b4b06f2f3584584fe34dcd9e71b060764ffccef8
William Patton <will@pattonwebz.com>
2021-07-23 17:01:45 +0100

    Add some basic styleing for nav items

--- src/layoutComponents/SiteNav.tsx
+++ src/layoutComponents/SiteNav.tsx
@@ -1,5 +1,7 @@
 import React, { CSSProperties } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import styled from 'styled-components';
 // import '../site-nav.css';
 
 const SiteNav = () => {
@@ -9,12 +11,39 @@ const SiteNav = () => {
         textDecoration: 'none'
     };
 
+    const SiteNav = styled.nav`
+        display: flex;
+        background: beige;
+        padding: 1rem 0.5rem;
+    `;
+
+    const SiteNavTitle = styled.div`
+        align-self: center;
+
+        h1 {
+            margin: 0 1rem;
+        }
+    `;
+
+    const SiteNavList = styled.ul`
+        display: flex;
+        list-style: none;
+    	margin: 0;
+        padding: 0;
+    	align-self: center;
+
+        li {
+            flex: 0 0 0;
+            margin-left: 1rem;
+        }
+    `;
+
     return (
-        <nav id="site-nav">
-            <div className="site-title">
+        <SiteNav id="site-nav">
+            <SiteNavTitle>
                 <h1>User Story Generator</h1>
-            </div>
-            <ul className="nav">
+            </SiteNavTitle>
+            <SiteNavList>
                 <li>
                     <NavLink
                         to="/"
@@ -32,8 +61,8 @@ const SiteNav = () => {
                         About
                     </NavLink>
                 </li>
-            </ul>
-        </nav>
+            </SiteNavList>
+        </SiteNav>
     );
 }
 
